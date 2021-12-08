3aeaa9eb3f38066f894c8ddeaaf1bb9a6ab9f002
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2021-12-08 08:44:44 -0500

    Adding support for ApplicationAppearence API

---
+++ packages/core/src/resources/ApplicationAppearance.ts
@@ -0,0 +1,45 @@
+import { BaseResource } from '@gitbeaker/requester-utils';
+import {
+  RequestHelper,
+  Sudo,
+  BaseRequestOptions,
+  GitlabAPIResponse,
+  ShowExpanded,
+} from '../infrastructure';
+
+export interface ApplicationAppearenceSchema extends Record<string, unknown> {
+  title: string;
+  description: string;
+  logo: string;
+  header_logo: string;
+  favicon: string;
+  new_project_guidelines: string;
+  profile_image_guidelines: string;
+  header_message: string;
+  footer_message: string;
+  message_background_color: string;
+  message_font_color: string;
+  email_header_and_footer_enabled: boolean;
+}
+
+export class ApplicationSettings<C extends boolean = false> extends BaseResource<C> {
+  show<E extends boolean = false>(
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<ApplicationAppearenceSchema, C, E, never>> {
+    return RequestHelper.get<ApplicationAppearenceSchema>()(
+      this,
+      'application/appearence',
+      options,
+    );
+  }
+
+  edit<E extends boolean = false>(
+    options?: BaseRequestOptions<E>,
+  ): Promise<GitlabAPIResponse<ApplicationAppearenceSchema, C, E, never>> {
+    return RequestHelper.put<ApplicationAppearenceSchema>()(
+      this,
+      'application/appearence',
+      options,
+    );
+  }
+}
