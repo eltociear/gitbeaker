193ced501457b13b07dc9caba352afdb51cf28de
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2021-12-22 10:00:56 -0500

    Add support for ErrorTracking APIs

---
+++ packages/core/src/resources/ErrorTrackingClientKeys.ts
@@ -0,0 +1,51 @@
+import { BaseResource } from '@gitbeaker/requester-utils';
+import {
+  endpoint,
+  RequestHelper,
+  Sudo,
+  GitlabAPIResponse,
+  ShowExpanded,
+  PaginatedRequestOptions,
+} from '../infrastructure';
+
+export interface ErrorTrackingClientKeySchema extends Record<string, unknown> {
+  id: number;
+  active: boolean;
+  public_key: string;
+  sentry_dsn: string;
+}
+
+export class ApplicationSettings<C extends boolean = false> extends BaseResource<C> {
+  all<E extends boolean = false, P extends 'keyset' | 'offset' = 'keyset'>(
+    projectId: string | number,
+    options?: PaginatedRequestOptions<E, P>,
+  ): Promise<GitlabAPIResponse<ErrorTrackingClientKeySchema[], C, E, P>> {
+    return RequestHelper.get<ErrorTrackingClientKeySchema[]>()(
+      this,
+      endpoint`projects/${projectId}/error_tracking/client_keys`,
+      options,
+    );
+  }
+
+  create<E extends boolean = false>(
+    projectId: string | number,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<ErrorTrackingClientKeySchema, C, E, void>> {
+    return RequestHelper.post<ErrorTrackingClientKeySchema>()(
+      this,
+      endpoint`projects/${projectId}/error_tracking/client_keys`,
+      options,
+    );
+  }
+
+  remove<E extends boolean = false>(
+    projectId: string | number,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<void, C, E, void>> {
+    return RequestHelper.del()(
+      this,
+      endpoint`projects/${projectId}/error_tracking/client_keys`,
+      options,
+    );
+  }
+}
---
+++ packages/core/src/resources/ErrorTrackingSettings.ts
@@ -0,0 +1,48 @@
+import { BaseResource } from '@gitbeaker/requester-utils';
+import {
+  endpoint,
+  RequestHelper,
+  Sudo,
+  BaseRequestOptions,
+  GitlabAPIResponse,
+  ShowExpanded,
+} from '../infrastructure';
+
+export interface ErrorTrackingSettingsSchema extends Record<string, unknown> {
+  active: boolean;
+  project_name: string;
+  sentry_external_url: string;
+  api_url: string;
+  integrated: boolean;
+}
+
+export class ApplicationSettings<C extends boolean = false> extends BaseResource<C> {
+  show<E extends boolean = false>(
+    projectId: string | number,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<ErrorTrackingSettingsSchema, C, E, void>> {
+    return RequestHelper.get<ErrorTrackingSettingsSchema>()(
+      this,
+      endpoint`projects/${projectId}/error_tracking/settings`,
+      options,
+    );
+  }
+
+  edit<E extends boolean = false>(
+    projectId: string | number,
+    active: boolean,
+    { integrated, ...options }: BaseRequestOptions<E> = {},
+  ): Promise<GitlabAPIResponse<ErrorTrackingSettingsSchema, C, E, void>> {
+    return RequestHelper.patch<ErrorTrackingSettingsSchema>()(
+      this,
+      endpoint`projects/${projectId}/error_tracking/settings`,
+      {
+        query: {
+          active,
+          integrated,
+        },
+        ...options,
+      },
+    );
+  }
+}
