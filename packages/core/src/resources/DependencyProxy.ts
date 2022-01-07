ec36f7da29fe1c4dfeb52f74a33e2113ac7cba7e
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2022-01-07 17:25:44 -0500

    Adding support for Dependency Proxy

---
+++ packages/core/src/resources/DependencyProxy.ts
@@ -0,0 +1,12 @@
+import { BaseResource } from '@gitbeaker/requester-utils';
+import { RequestHelper } from '../infrastructure';
+import type { Sudo, ShowExpanded, GitlabAPIResponse } from '../infrastructure';
+
+export class DashboardAnnotations<C extends boolean = false> extends BaseResource<C> {
+  remove<E extends boolean = false>(
+    groupId: string | number,
+    options?: Sudo & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<unknown, C, E, void>> {
+    return RequestHelper.post()(this, `groups/${groupId}/dependency_proxy/cache`, options);
+  }
+}
