c9916a2c68b2d7e13bd530aab34148f690e2f4f6
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2022-01-12 20:48:48 -0500

    Update lint API

--- packages/core/src/resources/Lint.ts
+++ packages/core/src/resources/Lint.ts
@@ -1,5 +1,6 @@
 import { BaseResource } from '@gitbeaker/requester-utils';
-import { BaseRequestOptions, RequestHelper } from '../infrastructure';
+import { endpoint, RequestHelper } from '../infrastructure';
+import type { BaseRequestOptions, GitlabAPIResponse } from '../infrastructure';
 
 export interface LintSchema extends Record<string, unknown> {
   status: string;
@@ -13,20 +14,45 @@ export interface ContextualLintSchema extends Omit<LintSchema, 'status'> {
 }
 
 export class Lint<C extends boolean = false> extends BaseResource<C> {
-  lint(content: string, options?: BaseRequestOptions) {
-    // Perform CI file linting without context.
-    // See https://docs.gitlab.com/ee/api/lint.html#validate-the-ci-yaml-configuration
-    // This API doesn't work for CI files that contain `local` includes. Use `lintWithNamespace` instead.
-    return RequestHelper.post<LintSchema>()(this, 'ci/lint', { content, ...options });
+  check<E extends boolean = false>(
+    projectId: string | number,
+    options: BaseRequestOptions<E>,
+  ): Promise<GitlabAPIResponse<ContextualLintSchema, C, E, void>> {
+    return RequestHelper.get<ContextualLintSchema>()(
+      this,
+      endpoint`projects/${projectId}/ci/lint`,
+      options,
+    );
   }
 
-  // TODO: Figure out a better way of writing this to not be limited by the typing.
-  lintWithNamespace(projectId: string | number, content: string, options?: BaseRequestOptions) {
-    // Perform CI file linting in the context of a specific project namespace.
-    // See https://docs.gitlab.com/ee/api/lint.html#validate-a-ci-yaml-configuration-with-a-namespace
-    // This API is useful when the CI file being linted has `local` includes, which requires project
-    // context to be understood.
-    return RequestHelper.post<ContextualLintSchema>()(this, `projects/${projectId}/ci/lint`, {
+  lint<E extends boolean = false>(
+    content: string,
+    options?: BaseRequestOptions<E>,
+  ): Promise<GitlabAPIResponse<LintSchema, C, E, void>>;
+  lint<E extends boolean = false>(
+    content: string,
+    options?: { projectId: string | number } & BaseRequestOptions<E>,
+  ): Promise<GitlabAPIResponse<ContextualLintSchema, C, E, void>>;
+  lint<E extends boolean = false>(
+    content: string,
+    { projectId, ...options }: { projectId?: string | number } & BaseRequestOptions<E> = {},
+  ): Promise<GitlabAPIResponse<LintSchema | ContextualLintSchema, C, E, void>> {
+    let url: string;
+
+    if (projectId) {
+      // Perform CI file linting in the context of a specific project namespace.
+      // See https://docs.gitlab.com/ee/api/lint.html#validate-a-ci-yaml-configuration-with-a-namespace
+      // This API is useful when the CI file being linted has `local` includes, which requires project
+      // context to be understood.
+      url = endpoint`projects/${projectId}/ci/lint`;
+    } else {
+      // Perform CI file linting without context.
+      // See https://docs.gitlab.com/ee/api/lint.html#validate-the-ci-yaml-configuration
+      // This API doesn't work for CI files that contain `local` includes. Use `lintWithNamespace` instead.
+      url = 'ci/lint';
+    }
+
+    return RequestHelper.post<LintSchema | ContextualLintSchema>()(this, url, {
       content,
       ...options,
     });
