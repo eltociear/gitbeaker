1a931544593d46b9f2c2540e733013ce11d70bb0
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2022-01-12 20:37:05 -0500

    Adding composer and conan support

---
+++ packages/core/src/resources/Composer.ts
@@ -0,0 +1,134 @@
+import { BaseResource } from '@gitbeaker/requester-utils';
+import { endpoint, RequestHelper } from '../infrastructure';
+import type { ShowExpanded, GitlabAPIResponse } from '../infrastructure';
+
+export interface ComposerV1BaseRepositorySchema extends Record<string, unknown> {
+  packages?: string[];
+  'metadata-url': string;
+  'provider-includes': Record<string, Record<string, string>>;
+  'providers-url': string;
+}
+
+export interface ComposerV2BaseRepositorySchema extends Record<string, unknown> {
+  packages?: string[];
+  'metadata-url': string;
+}
+
+export interface ComposerV1PackagesSchema extends Record<string, unknown> {
+  providers: {
+    [name: string]: {
+      sha256: string;
+    };
+  };
+}
+
+export interface PackageMetadata {
+  name: string;
+  type: string;
+  license: string;
+  version: string;
+  dist: {
+    type: string;
+    url: string;
+    reference: string;
+    shasum: string;
+  };
+  source: {
+    type: string;
+    url: string;
+    reference: string;
+  };
+  uid: number;
+}
+
+export interface ComposerPackageMetadataSchema extends Record<string, unknown> {
+  packages: {
+    [name: string]: {
+      [version: string]: PackageMetadata;
+    };
+  };
+}
+
+export class Composer<C extends boolean = false> extends BaseResource<C> {
+  create<E extends boolean = false>(
+    projectId: string | number,
+    options?: { tag?: string; branch?: string } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<{ message: string }, C, E, void>> {
+    return RequestHelper.post<{ message: string }>()(
+      this,
+      endpoint`projects/${projectId}/packages/composer`,
+      options,
+    );
+  }
+
+  download<E extends boolean = false>(
+    projectId: string | number,
+    packageName: string,
+    sha: string,
+    options?: ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<Blob, C, E, void>> {
+    return RequestHelper.get<Blob>()(
+      this,
+      endpoint`projects/${projectId}/packages/composer/archives/${packageName}`,
+      {
+        query: { sha },
+        ...options,
+      },
+    );
+  }
+
+  metadata<E extends boolean = false>(
+    groupId: string | number,
+    packageName: string,
+    options?: { sha?: string } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<ComposerPackageMetadataSchema, C, E, void>> {
+    let url: string;
+
+    if (options?.sha) {
+      url = endpoint`groups/${groupId}/-/packages/composer/${packageName}$${options.sha}`;
+    } else {
+      url = endpoint`groups/${groupId}/-/packages/composer/p2/${packageName}`;
+    }
+
+    return RequestHelper.get<ComposerPackageMetadataSchema>()(this, url, options);
+  }
+
+  packages<E extends boolean = false>(
+    groupId: string | number,
+    sha: string,
+    options?: ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<ComposerV1PackagesSchema, C, E, void>> {
+    return RequestHelper.get<ComposerV1PackagesSchema>()(
+      this,
+      endpoint`groups/${groupId}/-/packages/composer/p/${sha}`,
+      options,
+    );
+  }
+
+  showBaseRepository<E extends boolean = false>(
+    groupId: string | number,
+    options?: { composerVersion: '1' } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<ComposerV1BaseRepositorySchema, C, E, void>>;
+  showBaseRepository<E extends boolean = false>(
+    groupId: string | number,
+    options?: { composerVersion: '2' } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<ComposerV2BaseRepositorySchema, C, E, void>>;
+  showBaseRepository<E extends boolean = false>(
+    groupId: string | number,
+    options?: { composerVersion?: '1' | '2' } & ShowExpanded<E>,
+  ): Promise<
+    GitlabAPIResponse<ComposerV1BaseRepositorySchema | ComposerV2BaseRepositorySchema, C, E, void>
+  > {
+    const clonedService = { ...this };
+
+    if (options?.composerVersion === '2') {
+      clonedService.headers['User-Agent'] = 'Composer/2';
+    }
+
+    return RequestHelper.get<ComposerV1BaseRepositorySchema | ComposerV2BaseRepositorySchema>()(
+      clonedService,
+      endpoint`groups/${groupId}/-/packages/composer/packages`,
+      options,
+    );
+  }
+}
---
+++ packages/core/src/resources/Conan.ts
@@ -0,0 +1,306 @@
+import * as Mime from 'mime/lite';
+import { BaseResource } from '@gitbeaker/requester-utils';
+import { endpoint, RequestHelper } from '../infrastructure';
+import type { ShowExpanded, GitlabAPIResponse } from '../infrastructure';
+
+export interface PackageSnapshotSchema extends Record<string, unknown> {
+  'conan_package.tgz': string;
+  'conanfile.py': string;
+  'conanmanifest.txt': string;
+}
+
+export interface RecipeSnapshotSchema extends Record<string, unknown> {
+  'conan_sources.tgz': string;
+  'conanfile.py': string;
+  'conanmanifest.txt': string;
+}
+
+function url(projectId?: string | number) {
+  if (projectId) {
+    return endpoint`/projects/${projectId}/packages/conan/v1`;
+  } else {
+    return '/packages/conan/v1';
+  }
+}
+
+export class Conan<C extends boolean = false> extends BaseResource<C> {
+  authenticate<E extends boolean = false>(
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<string, C, E, void>> {
+    return RequestHelper.get<string>()(
+      this,
+      `${url(options?.projectId)}/users/authenticate`,
+      options,
+    );
+  }
+
+  checkCredentials<E extends boolean = false>(
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<string, C, E, void>> {
+    return RequestHelper.get<string>()(
+      this,
+      `${url(options?.projectId)}/users/check_credentials`,
+      options,
+    );
+  }
+
+  downloadPackageFile<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    conanPackageReference: string,
+    recipeRevision: string,
+    packageRevision: string,
+    filename: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<Blob, C, E, void>> {
+    return RequestHelper.get<Blob>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/v1/files/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/${recipeRevision}/package/${conanPackageReference}/${packageRevision}/${filename}`,
+      options,
+    );
+  }
+
+  downloadRecipeFile<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    recipeRevision: string,
+    filename: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<Blob, C, E, void>> {
+    return RequestHelper.get<Blob>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/v1/files/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/${recipeRevision}/export/${filename}`,
+      options,
+    );
+  }
+
+  packageUploadUrls<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    conanPackageReference: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<PackageSnapshotSchema, C, E, void>> {
+    return RequestHelper.get<PackageSnapshotSchema>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/packages/${conanPackageReference}/upload_urls`,
+      options,
+    );
+  }
+
+  packageDownloadUrls<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    conanPackageReference: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<PackageSnapshotSchema, C, E, void>> {
+    return RequestHelper.get<PackageSnapshotSchema>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/packages/${conanPackageReference}/download_urls`,
+      options,
+    );
+  }
+
+  packageManifest<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    conanPackageReference: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<PackageSnapshotSchema, C, E, void>> {
+    return RequestHelper.get<PackageSnapshotSchema>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/packages/${conanPackageReference}/digest`,
+      options,
+    );
+  }
+
+  packageSnapshot<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    conanPackageReference: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<PackageSnapshotSchema, C, E, void>> {
+    return RequestHelper.get<PackageSnapshotSchema>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/packages/${conanPackageReference}`,
+      options,
+    );
+  }
+
+  ping<E extends boolean = false>(
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<string, C, E, void>> {
+    return RequestHelper.post<string>()(this, `${url(options?.projectId)}/ping`, options);
+  }
+
+  recipeUploadUrls<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<RecipeSnapshotSchema, C, E, void>> {
+    return RequestHelper.get<RecipeSnapshotSchema>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/upload_urls`,
+      options,
+    );
+  }
+
+  recipeDownloadUrls<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<RecipeSnapshotSchema, C, E, void>> {
+    return RequestHelper.get<RecipeSnapshotSchema>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/download_urls`,
+      options,
+    );
+  }
+
+  recipeManifest<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<RecipeSnapshotSchema, C, E, void>> {
+    return RequestHelper.get<RecipeSnapshotSchema>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/digest`,
+      options,
+    );
+  }
+
+  recipeSnapshot<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<RecipeSnapshotSchema, C, E, void>> {
+    return RequestHelper.get<RecipeSnapshotSchema>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}`,
+      options,
+    );
+  }
+
+  removePackageFile<E extends boolean = false>(
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<void, C, E, void>> {
+    return RequestHelper.get<void>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}`,
+      options,
+    );
+  }
+
+  search<E extends boolean = false>(
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<{ results: string[] }, C, E, void>> {
+    return RequestHelper.get<{ results: string[] }>()(
+      this,
+      `${url(options?.projectId)}/conans/search`,
+      options,
+    );
+  }
+
+  uploadPackageFile<E extends boolean = false>(
+    content: string,
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    conanPackageReference: string,
+    recipeRevision: string,
+    packageRevision: string,
+    filename: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<unknown, C, E, void>> {
+    const meta = {
+      filename,
+      contentType: Mime.getType(filename),
+    };
+
+    return RequestHelper.get<Blob>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/v1/files/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/${recipeRevision}/package/${conanPackageReference}/${packageRevision}/${filename}`,
+      {
+        isForm: true,
+        ...options,
+        file: [content, meta],
+      },
+    );
+  }
+
+  uploadRecipeFile<E extends boolean = false>(
+    content: string,
+    packageName: string,
+    packageVersion: string,
+    packageUsername: string,
+    packageChannel: string,
+    recipeRevision: string,
+    filename: string,
+    options?: { projectId?: string | number } & ShowExpanded<E>,
+  ): Promise<GitlabAPIResponse<unknown, C, E, void>> {
+    const meta = {
+      filename,
+      contentType: Mime.getType(filename),
+    };
+
+    return RequestHelper.get<unknown>()(
+      this,
+      `${url(
+        options?.projectId,
+      )}/conans/v1/files/${packageName}/${packageVersion}/${packageUsername}/${packageChannel}/${recipeRevision}/export/${filename}`,
+      {
+        isForm: true,
+        ...options,
+        file: [content, meta],
+      },
+    );
+  }
+}
