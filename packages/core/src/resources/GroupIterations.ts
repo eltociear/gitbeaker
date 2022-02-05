6dc1c89fd55653ced4ee89130f3f9ebd205caae4
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2022-02-04 20:33:52 -0500

    Adding missing types overrides

--- packages/core/src/resources/GroupIterations.ts
+++ packages/core/src/resources/GroupIterations.ts
@@ -1,5 +1,18 @@
 import type { BaseResourceOptions } from '@gitbeaker/requester-utils';
 import { ResourceIterations } from '../templates';
+import type { IterationSchema } from '../templates/types';
+import type { BaseRequestOptions, GitlabAPIResponse } from '../infrastructure';
+
+export interface GroupIterations<C extends boolean = false> {
+  all<E extends boolean = false>(
+    groupId: string | number,
+    options?: {
+      state?: 'opened' | 'upcoming' | 'current' | 'closed' | 'all';
+      search?: string;
+      includeAncestors?: boolean;
+    } & BaseRequestOptions<E>,
+  ): Promise<GitlabAPIResponse<IterationSchema[], C, E, void>>
+}
 
 export class GroupIterations<C extends boolean = false> extends ResourceIterations<C> {
   constructor(options: BaseResourceOptions<C>) {
--- packages/core/src/resources/ProjectIterations.ts
+++ packages/core/src/resources/ProjectIterations.ts
@@ -1,5 +1,18 @@
 import type { BaseResourceOptions } from '@gitbeaker/requester-utils';
 import { ResourceIterations } from '../templates';
+import type { IterationSchema } from '../templates/types';
+import type { BaseRequestOptions, GitlabAPIResponse } from '../infrastructure';
+
+export interface ProjectIterations<C extends boolean = false> {
+  all<E extends boolean = false>(
+    projectId: string | number,
+    options?: {
+      state?: 'opened' | 'upcoming' | 'current' | 'closed' | 'all';
+      search?: string;
+      includeAncestors?: boolean;
+    } & BaseRequestOptions<E>,
+  ): Promise<GitlabAPIResponse<IterationSchema[], C, E, void>>
+}
 
 export class ProjectIterations<C extends boolean = false> extends ResourceIterations<C> {
   constructor(options: BaseResourceOptions<C>) {
